356940a66acd80566ff33b99fa9a40addc32c71a
MakarytskyiO <O.Makarytsky@deltabank.com.ua>
2015-02-17 18:05:24 +0200

    level 21 lesson02 task 1-3

---
+++ src/main/java/com/javarush/test/level21/lesson02/task01/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level21.lesson02.task01;
+
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+
+/* Определяем адрес сети
+1) Даны IP-адрес и маска подсети, необходимо вычислить адрес сети - метод getNetAddress.
+Используйте операцию поразрядной конъюнкции (логическое И).
+Пример:
+IP-адрес:       11000000 10101000 00000001 00000010 (192.168.1.2)
+Маска подсети:  11111111 11111111 11111110 00000000 (255.255.254.0)
+Адрес сети:     11000000 10101000 00000000 00000000 (192.168.0.0)
+2) Реализовать метод print, который выведет в консоль данные в двоичном коде
+3) Метод main не участвует в тестировании
+*/
+public class Solution {
+    public static void main(String[] args) {
+        byte[] ip = new byte[]{(byte) 192, (byte) 168, 1, 2};
+        byte[] mask = new byte[]{(byte) 255, (byte) 255, (byte) 254, 0};
+        byte[] netAddress = getNetAddress(ip, mask);
+        print(ip);          //11000000 10101000 00000001 00000010
+        print(mask);        //11111111 11111111 11111110 00000000
+        print(netAddress);  //11000000 10101000 00000000 00000000
+    }
+
+    public static byte[] getNetAddress(byte[] ip, byte[] mask) {
+        int ipInt = ByteBuffer.wrap(ip).getInt();
+        int maskInt = ByteBuffer.wrap(mask).getInt();
+        int res = ipInt & maskInt;
+        byte[] netAddress = ByteBuffer.allocate(4).putInt(res).array();
+        return netAddress;
+    }
+
+    public static void print(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0')).append(" ");
+        }
+        sb.replace(sb.length() - 1, sb.length(), "");
+        System.out.println(sb.toString());
+    }
+}
