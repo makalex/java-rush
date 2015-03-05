fceac53dc65468c35dbbdd4098d482dd7ad89fe1
makalex <henkukr@gmail.com>
2015-03-05 23:56:18 +0200

    level30 lesson02 home01

--- src/main/java/com/javarush/test/level30/lesson02/home01/Solution.java
+++ src/main/java/com/javarush/test/level30/lesson02/home01/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level30.lesson02.home01;
 
+import java.math.BigInteger;
+
 /* Конвертер систем счислений
 Реализуйте логику метода convertNumberToOtherNumerationSystem, который должен переводить число number.getDigit()
 из одной системы счисления(numerationSystem) в другую (expectedNumerationSystem)
@@ -9,13 +11,15 @@ package com.javarush.test.level30.lesson02.home01;
 */
 public class Solution {
     public static void main(String[] args) {
+        //Number number = new Number(NumerationSystemType._2, "1101001000000001100001001110110111111100110010101000100111011011011001001011001100011001100000111101111");
         Number number = new Number(NumerationSystemType._10, "6");
         Number result = convertNumberToOtherNumerationSystem(number, NumerationSystemType._2);
         System.out.println(result);    //expected 110
     }
 
     public static Number convertNumberToOtherNumerationSystem(Number number, NumerationSystem expectedNumerationSystem) {
-        number.getDigit();
-        return null;
+        BigInteger bigInteger = new BigInteger(number.getDigit(), number.getNumerationSystem().getNumerationSystemIntValue());
+        String str = bigInteger.toString(expectedNumerationSystem.getNumerationSystemIntValue());
+        return new Number(expectedNumerationSystem, str);
     }
 }
